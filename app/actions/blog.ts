f8026b3cb36d606d19279ab6770b826fdc8902c0
yeji yu <yeji.yu@concentrix.com>
2025-05-20 14:36:07 +0900

    #13.4 서버 액션 (Server Actions) 활용

--- app/actions/blog.ts
+++ app/actions/blog.ts
@@ -3,10 +3,15 @@
 import { createPost } from '@/lib/notion';
 
 export async function createPostAction(formData: FormData) {
-  const title = formData.get('title') as string;
-  const tag = formData.get('tag') as string;
-  const content = formData.get('content') as string;
+  // const title = formData.get('title') as string;
+  // const tag = formData.get('tag') as string;
+  // const content = formData.get('content') as string;
 
-  await createPost({ title, tag, content });
-  console.log('포스트 생성 완료');
+  const { title, tag, content } = Object.fromEntries(formData);
+
+  await createPost({
+    title: String(title),
+    tag: String(tag),
+    content: String(content),
+  });
 }
